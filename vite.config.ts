f3987c9b9ca07fe0353bb662b4f6c4b14752fc13
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2026-03-23 01:35:31 +0900

    ver.1.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isUserPage = repoName?.match(/\.github\.io$/);
+
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: process.env.GITHUB_REPOSITORY ? (isUserPage ? '/' : `/${repoName}/`) : '/',
 })
