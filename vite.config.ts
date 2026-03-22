0325e43c74466f25ae6276f69aebe988a071b71c
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2026-03-23 02:31:04 +0900

    ver.1.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-const isUserPage = repoName?.match(/\.github\.io$/);
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_REPOSITORY ? (isUserPage ? '/' : `/${repoName}/`) : '/',
+  base: '/MORTM_Scaling/',
 })
